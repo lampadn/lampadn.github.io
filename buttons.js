af3380db9b97a687d06f222e9b215846f87a5ead
lampadn <lisiyizamik@yandex.ru>
2026-01-07 01:03:05 +0300

    buttons.js

--- buttons.js
+++ buttons.js
@@ -625,7 +625,7 @@
                 'gap: 0.5em !important; ' +
             '}' +
             '.full-start-new__buttons.buttons-loading .full-start__button { visibility: hidden !important; }' +
-            '.folder-reset-button { background: rgba(0, 0, 255, 0.5); margin-top: 1em; border-radius: 0.3em; }' +
+            '.folder-reset-button { background: rgba(200,100,100,0.3); margin-top: 1em; border-radius: 0.3em; }' +
             '.folder-reset-button.focus { border: 3px solid rgba(255,255,255,0.8); }' +
             '.menu-edit-list__toggle.focus { border: 2px solid rgba(255,255,255,0.8); border-radius: 0.3em; }' +
             '.full-start-new__buttons.icons-only .full-start__button span { display: none; }' +
