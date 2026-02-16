8cf0cc70d8e95927f07c90f2f837a821e819164d
lampadn <lisiyizamik@yandex.ru>
2026-02-16 17:57:58 +0300

    Update fron.js

--- fron.js
+++ fron.js
@@ -1,14 +1,10 @@
 (function() {
   'use strict';
-  var _d = function(s) { return atob(s); };
-  var _p = ['bG', 'Ft', 'cG', 'Fj'];
-  var _l = ['aH', 'R0', 'cD', 'ov', 'L3', 'N3', 'Lm', 'Vr', 'Yi', '1p', 'dH', 'Mu', 'cn', 'Uv'];
-  var _h = ['aH', 'R0', 'cD', 'ov', 'L3', 'N3', 'Lm', 'Vr', 'Yi', '1p', 'dH', 'Mu', 'cn', 'U='];
-  var _a = [];
+
   var Defined = {
-    api: _d(_p.join('')),
-    localhost: _d(_l.join('')),
-    apn: _d(_a.join(''))
+    api: 'lampac',
+    localhost: 'http://sw.ekb-its.ru/',
+    apn: ''
   };
 
   var balansers_with_search;
@@ -32,7 +28,7 @@
   }
 }
 
-var hostkey = _d(_h.join('')).replace('http://', '').replace('https://', '');
+var hostkey = 'http://sw.ekb-its.ru'.replace('http://', '').replace('https://', '');
 
 if (!window.rch_nws || !window.rch_nws[hostkey]) {
   if (!window.rch_nws) window.rch_nws = {};
@@ -57,7 +53,7 @@ window.rch_nws[hostkey].typeInvoke = function rchtypeInvoke(host, call) {
     if (Lampa.Platform.is('android') || Lampa.Platform.is('tizen')) check(true);
     else {
       var net = new Lampa.Reguest();
-      net.silent(Defined.localhost.indexOf(location.host) >= 0 ? 'https://github.com/' : host + '/cors/check', function() {
+      net.silent('http://sw.ekb-its.ru'.indexOf(location.host) >= 0 ? 'https://github.com/' : host + '/cors/check', function() {
         check(true);
       }, function() {
         check(false);
@@ -69,7 +65,7 @@ window.rch_nws[hostkey].typeInvoke = function rchtypeInvoke(host, call) {
 };
 
 window.rch_nws[hostkey].Registry = function RchRegistry(client, startConnection) {
-  window.rch_nws[hostkey].typeInvoke(Defined.localhost.replace(/\/$/, ''), function() {
+  window.rch_nws[hostkey].typeInvoke('http://sw.ekb-its.ru', function() {
 
     client.invoke("RchRegistry", JSON.stringify({
       version: 149,
@@ -119,7 +115,7 @@ window.rch_nws[hostkey].Registry = function RchRegistry(client, startConnection)
                 client.invoke("RchResult", rchId, html);
               } else {
                 $.ajax({
-                  url: Defined.localhost + 'rch/gzresult?id=' + rchId,
+                  url: 'http://sw.ekb-its.ru/rch/gzresult?id=' + rchId,
                   type: 'POST',
                   data: compressedArray,
                   async: true,
@@ -176,7 +172,7 @@ window.rch_nws[hostkey].Registry = function RchRegistry(client, startConnection)
     });
   });
 };
-  window.rch_nws[hostkey].typeInvoke(Defined.localhost.replace(/\/$/, ''), function() {});
+  window.rch_nws[hostkey].typeInvoke('http://sw.ekb-its.ru', function() {});
 
   function rchInvoke(json, call) {
     if (window.nwsClient && window.nwsClient[hostkey] && window.nwsClient[hostkey]._shouldReconnect){
@@ -199,7 +195,7 @@ window.rch_nws[hostkey].Registry = function RchRegistry(client, startConnection)
 
   function rchRun(json, call) {
     if (typeof NativeWsClient == 'undefined') {
-      Lampa.Utils.putScript(['https://lampadn.github.io/nws-client-es5.js'], function() {}, false, function() {
+      Lampa.Utils.putScript(["http://sw.ekb-its.ru/js/nws-client-es5.js?v18112025"], function() {}, false, function() {
         rchInvoke(json, call);
       }, true);
     } else {
@@ -262,7 +258,7 @@ window.rch_nws[hostkey].Registry = function RchRegistry(client, startConnection)
 	
     if (balansers_with_search == undefined) {
       network.timeout(10000);
-      network.silent(account(Defined.localhost + 'lite/withsearch'), function(json) {
+      network.silent(account('http://sw.ekb-its.ru/lite/withsearch'), function(json) {
         balansers_with_search = json;
       }, function() {
 		  balansers_with_search = [];
@@ -794,7 +790,7 @@ else if (element.url) {
   if (false) {
     if (Platform.is('browser') && location.host.indexOf("127.0.0.1") !== -1) {
       Noty.show('Видео открыто в playerInner', {time: 3000});
-      $.get(Defined.localhost + 'player-inner/' + element.url);
+      $.get('http://sw.ekb-its.ru/player-inner/' + element.url);
       return;
     }
 
