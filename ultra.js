9d6ed7a301982b76a0854fbb8015f5eb03fcd8d2
lampadn <lisiyizamik@yandex.ru>
2026-05-24 13:19:43 +0300

    Update ultra.js

--- ultra.js
+++ ultra.js
@@ -93,7 +93,7 @@
         if (connection_source === 'showy') return MIRRORS_SHOWY[current_showy_index];
         if (connection_source === 'okeantv') return 'http://148.135.207.174:12359/';
         if (connection_source === 'hdpoisk') return 'https://hdpoisk.ru/';
-        if (connection_source === 'lampaua') return 'https://apn2.akter-black.com/http://lampaua.mooo.com/';
+        if (connection_source === 'lampaua') return 'http://lampaua.mooo.com/';
         if (connection_source === 'beta') return 'http://beta.l-vid.online';
         return randomUrl; // Skaz
     }
